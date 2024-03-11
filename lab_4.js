88102a7b756a261e4ff46966ff46f8b48d3075f1
Tshiya1 <160495821+Tshiya1@users.noreply.github.com>
2024-03-11 15:16:34 +0000

    Add files via upload

---
+++ lab_4.js
@@ -0,0 +1,84 @@
+const countries = "https://restcountries.com/v3.1/all";
+const countryName = document.getElementById("countryName");
+const button = document.getElementById("getCountryInfo");
+
+button.addEventListener("click", function getCountryInfo(){
+
+});
+
+async function fetchCountries(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      const countries = response.json();
+      findInfo(JSON.parse(countries));
+    } catch (error) {
+      console.error("There has been a problem with your fetch operation:", error);
+    }
+  }
+  
+function findInfo(countries,countryName){
+for(let i = 0; i<countries.length; i++){
+if(countries[i].names.common==countryName || countries[i].names.common == countryName){
+getInfo(countries[i]);
+getNeighbours(countries[i],countries);
+  break;
+}
+else{
+  continue;
+}
+}
+
+}
+
+function getInfo(country){
+const art = document.getElementById("countryInfo");
+const Capital = document.createElement('p');
+const Population = document.createElement('p');
+const Region = document.createElement('p');
+const Flag = document.createElement('img');
+
+Capital.textContent = 'Capital:'+country.name.common;
+Population.textContent = 'Population:'+country.population;
+Region.textContent = 'Region:'+country.region;
+Flag.src = country.flag.png;
+
+art.appendChild(Capital);
+art.appendChild(Population);
+art.appendChild(Region);
+art.appendChild(Flag);
+}
+
+function getNeighbours(country,countries){
+if(country.borders.length == 0){
+//no neighbours
+}
+else{
+const art2 = document.getElementById("BorderingCountries");
+for(let i = 0;i<country.borders.length;i++){
+
+parseNieghbour(country.borders[i],countries,art2);
+
+}
+}
+}
+
+function parseNieghbour(country,countries,art){
+
+  for(let i = 0; i<countries.length; i++){
+    if(countries[i].cca3 = country){
+      const cName = document.createElement('p');
+      const flag = document.createElement('img');
+
+      cName.textContent=countries[i].name.common;
+      flag.src = countries[i].flags.png;
+      art.appendChild(cName);
+      art.appendChild(flag);
+      break;
+    }
+    e
+    }
+}
+
